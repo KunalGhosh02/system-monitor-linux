79fb4fc3c3bd5e01b1fcc342be0d8d60bd6ae822
Kunal Ghosh <kunalghosh.res@gmail.com>
2020-05-22 22:09:55 +0530

    feat: set cpu utilization and return all other values from linux parser namespace

--- include/process.h
+++ include/process.h
@@ -8,16 +8,22 @@ It contains relevant attributes as shown below
 */
 class Process {
  public:
-  int Pid();                               // TODO: See src/process.cpp
-  std::string User();                      // TODO: See src/process.cpp
-  std::string Command();                   // TODO: See src/process.cpp
-  float CpuUtilization();                  // TODO: See src/process.cpp
-  std::string Ram();                       // TODO: See src/process.cpp
-  long int UpTime();                       // TODO: See src/process.cpp
-  bool operator<(Process const& a) const;  // TODO: See src/process.cpp
+  Process(const int pid);
+  void SetCpuUtilization(long active_ticks, long system_ticks);
+  int Pid();
+  std::string User();
+  std::string Command();
+  float CpuUtilization();
+  std::string Ram();
+  long int UpTime();
+  bool operator>(Process const& a) const;
 
-  // TODO: Declare any necessary private members
  private:
+  int pid_;
+  float cpu_utilization_;
+  long cached_active_ticks_;
+  long cached_idle_ticks_;
+  long cached_system_ticks_;
 };
 
 #endif
--- src/process.cpp
+++ src/process.cpp
@@ -7,30 +7,39 @@
 #include <string>
 #include <vector>
 
+#include "linux_parser.h"
+
 using std::string;
 using std::to_string;
 using std::vector;
+Process::Process(const int pid) : pid_(pid){};
+//  Return this process's ID
+int Process::Pid() { return pid_; }
 
-// TODO: Return this process's ID
-int Process::Pid() { return 0; }
+// Return this process's CPU utilization
+float Process::CpuUtilization() { return cpu_utilization_; }
 
-// TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+void Process::SetCpuUtilization(long active_ticks, long system_ticks) {
+  long duration_active{active_ticks - cached_active_ticks_};
+  long duration{system_ticks - cached_system_ticks_};
+  cpu_utilization_ = static_cast<float>(duration_active) / duration;
+  cached_active_ticks_ = active_ticks;
+  cached_system_ticks_ = system_ticks;
+}
 
-// TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+// Return the command that generated this process
+string Process::Command() { return LinuxParser::Command(pid_); }
 
-// TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+//  Return this process's memory utilization
+string Process::Ram() { return LinuxParser::Ram(pid_); }
 
-// TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+//  Return the user (name) that generated this process
+string Process::User() { return LinuxParser::User(pid_); }
 
-// TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+//  Return the age of this process (in seconds)
+long int Process::UpTime() { return LinuxParser::UpTime(pid_); }
 
-// TODO: Overload the "less than" comparison operator for Process objects
-// REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a [[maybe_unused]]) const {
-  return true;
+//  Overload the "less than" comparison operator for Process objects
+bool Process::operator>(Process const& a) const {
+  return (cpu_utilization_ > a.cpu_utilization_);
 }
