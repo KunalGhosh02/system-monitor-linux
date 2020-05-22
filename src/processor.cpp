aaacd8279b8e3e3ec5f03172fd6ee7e54d666d4b
Kunal Ghosh <kunalghosh.res@gmail.com>
2020-05-22 22:12:24 +0530

    feat: cpu utilization updates based on recent utilization

--- src/processor.cpp
+++ src/processor.cpp
@@ -4,10 +4,14 @@
 
 // TODO: Return the aggregate CPU utilization
 float Processor::Utilization() {
-  long ActiveJiffies = LinuxParser::ActiveJiffies();
-  long IdleJiffies = LinuxParser::IdleJiffies();
-
-  long TotalTime = ActiveJiffies + IdleJiffies;
-
-  return static_cast<float>(ActiveJiffies) / static_cast<float>(TotalTime);
+  double utilization{0};
+  long idle_ticks = LinuxParser::IdleJiffies();
+  long active_ticks = LinuxParser::ActiveJiffies();
+  long duration_activate{active_ticks - cached_active_ticks_};
+  long duration_idle{idle_ticks - cached_idle_ticks_};
+  long duration{duration_activate + duration_idle};
+  utilization = static_cast<double>(duration_activate) / duration;
+  cached_active_ticks_ = active_ticks;
+  cached_idle_ticks_ = idle_ticks;
+  return utilization;
 }
